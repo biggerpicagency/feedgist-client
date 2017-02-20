e41f7ad9d743c16ca917b1502f7813bd22c55454
Bartek Mis <bartek@studiosidekicks.com>
2017-02-20 23:51:42 +0100

    login with facebook implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { AuthService } from 'ng2-ui-auth';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+
+  constructor(private auth: AuthService) { }
+  
+  loginWithFacebook() {
+      this.auth.authenticate('facebook')
+          .subscribe((data) => {
+            console.log(data);
+          });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,14 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { Ng2UiAuthModule, CustomConfig } from 'ng2-ui-auth';
+
 import { AppComponent } from './app.component';
+
+export class MyAuthConfig extends CustomConfig {
+    defaultHeaders = {'Content-Type': 'application/json'};
+    providers = {facebook: {clientId: '1148407601949396', url: 'http://api.feed-gist.dev/api/auth/facebookCallback'}};
+}
 
 @NgModule({
   declarations: [
@@ -12,7 +19,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    Ng2UiAuthModule.forRoot(MyAuthConfig),
   ],
   providers: [],
   bootstrap: [AppComponent]
