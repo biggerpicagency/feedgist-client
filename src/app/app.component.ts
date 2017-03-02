cf12086a563fcdb8f35277dd8d6ee8cc130fec68
Bartek Mis <bartek@studiosidekicks.com>
2017-03-02 21:39:35 +0100

    conflict part 2 resolved

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,7 @@
 import { Component, Inject } from '@angular/core';
 
 @Component({
-<<<<<<< HEAD
   selector: 'app-root',
-=======
-  selector: 'app',
->>>>>>> 0c8e37e7a9e02e06db002e32c7109aab51fced42
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
