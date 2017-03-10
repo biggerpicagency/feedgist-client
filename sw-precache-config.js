f5ea53a7f0aa8e61c6a3b9885c4381aef51dfd3f
Bartek Mis <bartek@studiosidekicks.com>
2017-03-10 08:43:51 +0100

    offline support

---
+++ sw-precache-config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  staticFileGlobs: [
+    'dist/**.html',
+    'dist/**.js',
+    'dist/**.css',
+    'dist/assets/*',
+    'dist/*'
+  ],
+  root: 'dist',
+  stripPrefix: 'dist/',
+  navigateFallback: '/index.html',
+  runtimeCaching: [{
+    urlPattern: /api\.feed-gist\.dev/,
+    handler: 'networkFirst'
+  }, {
+    urlPattern: /external\.xx\.fbcdn\.net/,
+    handler: 'networkFirst'
+  }, {
+    urlPattern: /scontent\.xx\.fbcdn\.net/,
+    handler: 'networkFirst'
+  }, {
+    urlPattern: /fonts\.googleapis\.com/,
+    handler: 'networkFirst'
+  }, {
+    urlPattern: /fonts\.gstatic\.com/,
+    handler: 'networkFirst'
+  }]
+};
