1ff38c2bd8013bdda731cdd9992e61d216eadbba
Bartek Mis <bartek@studiosidekicks.com>
2017-03-19 17:01:50 +0100

    sw change

--- src/OneSignalSDKUpdaterWorker.js
+++ src/OneSignalSDKUpdaterWorker.js
@@ -1,4 +1,4 @@
-// service_worker - do not remove this line
-
 importScripts('service-worker.js');
 importScripts('https://cdn.onesignal.com/sdks/OneSignalSDK.js');
+
+// service_worker - do not remove this line
